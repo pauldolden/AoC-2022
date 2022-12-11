220a77801335831936fa5c0ddc06fcb586b0db6d
Paul Dolden <paul@dolden.co.uk>
2022-12-11 13:23:21 +0000

    did not enjoy, but day 8 complete

--- day8/day8.go
+++ day8/day8.go
@@ -22,65 +22,58 @@ type Position struct {
 
 func Day8() {
 	input := helpers.GetPuzzleInput("8")
-
-	var visible_trees int32 = 0
 	grid, height, width := buildGrid(input)
+	heighest_scenic_score := 0
 	// For each row
 	for y := 0; y < height; y++ {
 		// For each column
 		for x := 0; x < width; x++ {
 			// So for each tree we do the below
+			tree_score := []int{}
 			tree := grid[y][x]
 			// Is an edge or rather, is already visible just add a visible tree and move on
 			if y == 0 || x == 0 || y == height-1 || x == width-1 {
-				visible_trees++
 				continue
 			}
 			// It is not an edge so we want to check each direction
-		dir_loop: // For each direction
 			for _, dir := range directions {
 				x_move, y_move := dir[0], dir[1]
+				dir_score := 0
 				// if the tree is smaller than the one next to it (in that direction) just move on
 				// It is not visible in that direction
-				if grid[y+(y_move)][x+(x_move)] >= tree {
-					continue
-				}
 				// The tree is bigger than its neighbour so MIGHT be visible, so we need to keep checking this direction
-				// Until we reach an edge or a bigger tree
-
-				for k := 2; k < width; k++ {
+				// Until we reach an edge or a bigger tree_score
+				number_of_moves := 1
+				for k := 1; k <= width; k++ {
 					x_loc := x + x_move*k
 					y_loc := y + y_move*k
 					// Feels gross but just avoids index out of range errors
-					if x_loc >= width-1 {
+					if x_loc >= width {
 						x_loc = width - 1
 					}
 					if x_loc <= 0 {
 						x_loc = 0
 					}
-					if y_loc >= width-1 {
-						y_loc = width - 1
+					if y_loc >= height {
+						y_loc = height - 1
 					}
 					if y_loc <= 0 {
 						y_loc = 0
 					}
-					if grid[y_loc][x_loc] >= tree {
-						// If we arrive here, we've hit a bigger tree again so we can stop
-						// checking for bigger trees in this direction as this is not visible
+					if grid[y_loc][x_loc] >= tree || x_loc == 0 || x_loc == width-1 || y_loc == 0 || y_loc == height-1 {
 						break
-						// Break this and try another direction
 					}
-					if x_loc == 0 || x_loc == width-1 || y_loc == 0 || y_loc == height-1 {
-						visible_trees++
-						break dir_loop
-						// We've found a visible direction so we don't care if the other directions
-						// are visible so break out of the parent loop and move onto the next tree
-					}
+					number_of_moves++
 				}
+				dir_score = number_of_moves
+				tree_score = append(tree_score, dir_score)
+			}
+			if tree_score[0]*tree_score[1]*tree_score[2]*tree_score[3] > heighest_scenic_score {
+				heighest_scenic_score = tree_score[0] * tree_score[1] * tree_score[2] * tree_score[3]
 			}
 		}
 	}
-	fmt.Println(visible_trees)
+	fmt.Println(heighest_scenic_score)
 }
 
 func buildGrid(input string) (grid map[int][]int, height int, width int) {
